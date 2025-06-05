eae9eff4d035fd091948271d74c903095922b011
godspowerufot <ufotgodspower8@gmail.com>
2025-06-05 10:57:44 +0100

    feat: Update main heading font size for improved visibility on rent page

--- app/rent/page.tsx
+++ app/rent/page.tsx
@@ -60,7 +60,7 @@ export default function Home() {
         {/* Content Section */}
         <div className="flex flex-col items-center relative z-[1]  mt-[3rem] lg:mt-[6rem]   lg:gap-4 h-full ">
           {/* Main Heading */}
-          <h1 className="text-white text-center w-[20rem]   relative  font-bricolage font-semibold leading-tight  text-[32px] lg:text-[clamp(4em,4vw,4em)] lg:w-[60%] max-w-[700px] 2xl:max-w-[700px]">
+          <h1 className="text-white text-center w-[20rem]   relative  font-bricolage font-semibold leading-tight  text-[38px] lg:text-[clamp(4em,4vw,4em)] lg:w-[60%] max-w-[700px] 2xl:max-w-[700px]">
             Find Your Future, Feel at Home!{" "}
           </h1>
 
