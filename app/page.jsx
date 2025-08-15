f496e8a98b0e0cb9661b2fc3e17f638a888812b0
godspowerufot <ufotgodspower8@gmail.com>
2025-08-15 10:36:08 +0100

    made change

--- app/page.jsx
+++ app/page.jsx
@@ -446,7 +446,7 @@ https://expo.dev/artifacts/eas/fYMekk7hs69zo5CgvmfQ1N.apk"
         </div>
       </section>
 
-      <section className=" p-2 lg:p-0 lg:mb-[3em] w-full font-bricolage lg:flex justify-center flex-col flex-1 items-center">
+      <section className=" p-2 lg:p-0 lg:mb-[4.5em] w-full font-bricolage lg:flex justify-center flex-col flex-1 items-center">
         <div className="flex flex-col   items-start gap-6 lg:gap-0 justify-center max-w-[1200px] w-full">
           <div className="flex flex-col  lg:flex-row justify-between items-center w-full  mx-auto">
             <h1 className="text-black text-[24px] mt-[32px] lg:mt-0  lg:text-[2.5rem] font-[600] w-full lg:w-auto">
@@ -492,7 +492,7 @@ https://expo.dev/artifacts/eas/fYMekk7hs69zo5CgvmfQ1N.apk"
         </div>
       </section>
 
-      <section className="mt-5 w-screen lg:mb-[4em]   lg:w-full font-bricolage lg:flex justify-center flex-col flex-1 items-center">
+      <section className="mt-5 w-screen lg:mb-[5em]   lg:w-full font-bricolage lg:flex justify-center flex-col flex-1 items-center">
         <div className="flex flex-col items-start gap-6 lg:gap-0 justify-center max-w-[1200px] w-full">
           <div className="flex flex-col lg:py-0 lg:flex-row  p-3 lg:p-0 lg:gap-10 justify-between items-center w-full  mx-auto">
             <h1 className="text-black text-[24px] lg:text-[2.5rem] font-[600] w-full lg:w-auto">
@@ -513,7 +513,7 @@ https://expo.dev/artifacts/eas/fYMekk7hs69zo5CgvmfQ1N.apk"
       {/* carousel */}
 
       {/* testimonials */}
-      <section className=" p-2   pt-[2.5rem] lg:mb-[4em]  lg:p-0 font-bricolage  flex justify-center flex-col  items-center">
+      <section className=" p-2   pt-[2.5rem] lg:mb-[5em]  lg:p-0 font-bricolage  flex justify-center flex-col  items-center">
         <div className="flex  flex-col    lg:flex-row md:flex-row  lg:max-w-[1200px] lg:gap-0  justify-around items-start w-full   ">
           <span className="flex    2xl:w-full  flex-col font-bricolage gap-3">
             <h1 className="text-black   text-[26px] lg:text-[2.5rem] 2xl:text-5xl font-[600]  mr-5">
