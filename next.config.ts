a7e068f6ec97a340e28282c5f75ce9b9b12acad0
godspowerufot <ufotgodspower8@gmail.com>
2025-10-31 01:30:25 +0100

    made a chnage

--- next.config.ts
+++ next.config.ts
@@ -1,26 +1,29 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    // ✅ enables standalone output for production deployment
-    output: "standalone",
-    // ✅ enables gzip compression when using `next start`
-    compress: true,
-    images: {
-        remotePatterns: [
-            {
-                protocol: "https",
-                hostname: "hoydoonstorage.blob.core.windows.net",
-                port: "",
-                pathname: "/**", // Allows all paths from this hostname
-            },
-            {
-                protocol: "https",
-                hostname: "lh3.googleusercontent.com",
-                port: "",
-                pathname: "/**", // Google user profile images
-            },
-        ],
-    },
+  // ✅ enables standalone output for production deployment
+  output: "standalone",
+  // ✅ enables gzip compression when using `next start`
+  compress: true,
+  images: {
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    formats: ["image/webp"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "hoydoonstorage.blob.core.windows.net",
+        port: "",
+        pathname: "/**", // Allows all paths from this hostname
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        port: "",
+        pathname: "/**", // Google user profile images
+      },
+    ],
+  },
 };
 
 export default nextConfig;
